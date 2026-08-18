3bec9fca0d541e05c0ea2b4afadc11c19a3c69cc
darkeii <mauryashubhanshu1111@gmail.com>
2026-08-18 23:55:36 +0530

    day 6(1)

---
+++ python/exercises/Exercise11.py
@@ -0,0 +1,22 @@
+#Slot machine
+
+def spin_row():
+    pass
+
+def display_row():
+    pass
+
+def get_payout():
+    pass
+
+def main():
+    balance = 100           #starting money
+
+    print("Welcome to the python slots")
+    bet
+
+if __name__ == '__main__':
+    main()
+
+
+
