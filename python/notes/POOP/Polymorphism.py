5b032c51a68a56bad0e849dc7df58084ace73981
darkeii <mauryashubhanshu1111@gmail.com>
2026-08-30 21:52:18 +0530

    day 9(2)

---
+++ python/notes/POOP/Polymorphism.py
@@ -0,0 +1,48 @@
+# Polymorphism = Greek word that means to "have many forms or faces"
+#             Poly = Many
+#             Morphe = Form
+
+# TWO WAYS TO ACHIEVE POLYMORPHISM
+            # 1. Inheritence = An object could be treated of the same type as a parent class
+            # 2. "Duck typing" = object must have neccessary attributes/methods
+
+from abc import ABC, abstractmethod
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * pow(self.radius, 2)
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return pow(self.side, 2)
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+class Pizza(Circle):
+    def __init__(self, toppings, radius):
+        self.toppings = toppings
+        super().__init__(radius)
+
+shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
+
+for shape in shapes:
+    print(shape.area())
+
--- python/notes/POOP/super ( ).py
+++ python/notes/POOP/super ( ).py
@@ -1,4 +1,4 @@
-# super() = Function used in a chiled class to call methods from a parent class (superclass).
+# super() = Function used in a chiled class to call methods/attributes from a parent class (superclass).
 #             Allows you to extend the dunctionality of the inherited methods.
 
 class Shapes:
@@ -15,6 +15,10 @@ class Circle(Shapes):
         super().__init__(color, is_filled)
         self.radius = radius
 
+    def describe(self):                                                             # if a method of same name exists in both children calss and parent class, then children
+        print(f"This is a circle with area {3.14 * pow(self.radius, 2)}")           # class will have more priority
+        super().describe()
+
 class Square(Shapes):
     def __init__(self, color, is_filled, side):
         super().__init__(color, is_filled)
@@ -28,7 +32,7 @@ class Rectangle(Shapes):
 
 
 rectangle = Rectangle("green", True, 5, 4)
-circle = Circle("red", False, 1)
+circle = Circle("red", False, 2)
 square = Square("blue", True, 10)
 
 print(circle.color)
@@ -50,3 +54,5 @@ print(square.color)
 print(square.is_filled)
 print(square.side)
 square.describe()
+
+
