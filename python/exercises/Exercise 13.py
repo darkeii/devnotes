d6508edad288dc5d10347a5e69609259f993fe62
darkeii <mauryashubhanshu1111@gmail.com>
2026-09-12 17:46:32 +0530

    day 15

--- python/exercises/Exercise 13.py
+++ python/exercises/Exercise 13.py
@@ -1,31 +1,19 @@
-#Hangman game
+# Hangman game
 import random
+
 from words import basic_words
 
 # dictionary of key():
 hangman_art = {
-    0: ("   ",
-        "   ",
-        "   "),
-    1: (" O ",
-        "   ",
-        "   "),
-    2: (" O ",
-        " | ",
-        "   "),
-    3: (" O ",
-        " | ",
-        "/  "),
-    4: (" O ",
-        " | ",
-        "/ \\"),
-    5: (" O ",
-        "/| ",
-        "/ \\"),
-    6: (" O ",
-        "/|\\",
-        "/ \\")
+    0: ("   ", "   ", "   "),
+    1: (" O ", "   ", "   "),
+    2: (" O ", " | ", "   "),
+    3: (" O ", " | ", "/  "),
+    4: (" O ", " | ", "/ \\"),
+    5: (" O ", "/| ", "/ \\"),
+    6: (" O ", "/|\\", "/ \\"),
 }
+
 
 def display_man(wrong_guesses):
     print("***************************")
@@ -34,24 +22,26 @@ def display_man(wrong_guesses):
     print("***************************")
     print()
 
+
 def display_hint(hint):
     print(" ".join(hint))
 
+
 def display_answer(answer):
     print(f"Answer: {answer}")
+
 
 def main():
     answer = random.choice(basic_words)
     is_playing = True
     hint = ["_"] * len(answer)
     wrong_guesses = 0
-    guessed_letter = set()              #created an empty set (no duplicates allowed)
+    guessed_letter = set()  # created an empty set (no duplicates allowed)
 
     while is_playing:
         display_man(wrong_guesses)
         display_hint(hint)
         guess = input(": ").lower()
-
 
         if guess.isalpha() and len(guess) == 1:
             if guess in answer:
@@ -83,9 +73,5 @@ def main():
             is_playing = False
 
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ python/notes/pyQT/layouts.py
@@ -0,0 +1,23 @@
+# PyQt6 layouts
+import sys
+from PyQt6.QtWidgets import (QApplication,
+                             QMainWindow,
+                             QLabel,
+                             QWidget,
+                             QVBoxLayout,
+                             QHBoxLayout,
+                             QGridLayout )
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
